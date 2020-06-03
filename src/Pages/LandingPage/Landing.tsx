b65236c4c2f933775aedc30bceae039cb37882c7
litain <litain23@gmail.com>
2020-06-03 22:11:18 +0900

    Modify Landing page
    Using ant-design and styled-components, refactoring landing page

--- src/Pages/LandingPage/Landing.tsx
+++ src/Pages/LandingPage/Landing.tsx
@@ -1,66 +1,125 @@
 import React from 'react';
 import '../../App.css'
-import SearchBar from '../../R6Components/SearchBar/SearchBar'
-import Space from '../../R6Components/R6Br'
-import route from '../../route'
-import {RouteDecoratorProps} from '../../route'
-import {RouteComponentProps} from 'react-router-dom'
 
 import { Input } from 'antd';
 
+import styled from 'styled-components'
+
 const { Search } = Input;
+
+const StyledSearch = styled(Search)`
+    .ant-input-group * {
+        height: 60px;
+        font-size: 20px;
+        border-radius: 0px;
+    }
+
+    .ant-btn-primary {
+        background: #515163;
+        border-color: #515163;
+    }
+`;
+
+const LandingRoot = styled.div`
+    z-index: 10;
+`;
+
+const Video = styled.video`
+    width: 100%;
+    height: 100%;
+`;
+
+const Container = styled.div`
+    @media (min-width: 576px) {
+        max-width: 540px;
+    }
+
+    @media (min-width: 768px) {
+        max-width: 720px;
+    }
+
+    @media (min-width: 992px) {
+        max-width: 960px;
+    }
+
+    @media (min-width: 1200px) {
+        max-width: 1140px;
+    }
+
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+const HomeBackground = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    background: #1c1c1f;
+`;
+
+const HomeLogo = styled.div`
+    z-index: 11;
+    position: relative;
+    color: #fff;
+    display: flex;
+    height: 440px;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: 768px) {
+        height: 300px;
+    }
+
+`;
+
+const HomeSearch = styled.div`
+    z-index: 11;
+    height: 60px;
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+
+    padding: 0 100px;
+    @media (max-width: 768px) {
+        padding: 0 12px;
+    }
+`;
 
 export default class Landing extends React.Component {
     render() {
         return (
-            <div id="landing-root" style={{background: "#1c1c1f", zIndex: 10}}>
+            <LandingRoot>
+                <HomeBackground> </HomeBackground>
                 <div className="background-video" style={{position: "absolute", width: "100%", zIndex: 10, opacity: "0.3"}}>
-                    <video style={{width: "100%", height: "100%"}} id="myVideoContent" muted={true} autoPlay={true} loop={true} preload="auto" src="background_void_edge.mp4">
-                    </video>
+                    <Video id="background-video-content" muted={true} autoPlay={true} loop={true} preload="auto" src="background_void_edge.mp4">
+                    </Video>
                 </div>
 
-                <div className="logo-container" style={{
-                    zIndex: 11,
-                    height: "440px",
-                    width: "1140px", 
-                    position: "relative",
-                    color: "#fff",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                <Container>
+                    <HomeLogo>
+                        <span style={{
+                            fontSize: "40px",
+                        }}>
+                            Rain Box Six Seige : 
+                        </span>
+                    </HomeLogo>
+                </Container>
 
-                }}> 
-                    <span style={{
-                        fontSize: "40px",
-                    }}>
-                        Rain Box Six Seige : 
-                    </span>
-
-                </div>
-                <div className="searchbar-container" style={{
-                    zIndex: 11,
-                    width: "1140px", 
-                    height: "60px",
-                    position: "relative",
-                    padding: "0 100px",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    display:"border_box",
-                }}>
-                    <Search
-                        placeholder="아이디를 입력해주세요"
-                        enterButton="Go"
-                        size="large"
-                    />
-                </div>
-                <div className="container" style={{
-                    height: "calc(100vh - 500px)",
-
-                }}> 
-                </div>
-            </div>
+                <Container>
+                    <HomeSearch>
+                        <StyledSearch
+                            placeholder="아이디를 입력해주세요"
+                            enterButton="Go"
+                            size="large"
+                            style={{
+                                background: "#1c1c1f"
+                            }}
+                        />
+                    </HomeSearch>
+               </Container>
+            </LandingRoot>
         )
     }
 }
