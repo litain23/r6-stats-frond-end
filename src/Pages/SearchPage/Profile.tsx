6ff7a781a12126dd07174e8d836b6a1a3aee5c02
=litain <litain23@gmail.com>
2020-06-05 23:58:27 +0900

    Modify Profile and ProfileRank for mobile view
    플렉스를 이용해 모바일 환경에도 잘 보일 수 있게 변경

--- src/Pages/SearchPage/Profile.tsx
+++ src/Pages/SearchPage/Profile.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../../App.css'
 
-import Landing from './../LandingPage/Landing'
+import ProfileRank from './SubComponent/ProfileRank'
 
 import styled from 'styled-components'
-import { Tabs } from 'antd';
-
-const { TabPane } = Tabs;
 
 const Container = styled.div`
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+
     @media (min-width: 576px) {
         max-width: 540px;
     }
@@ -24,38 +25,77 @@ const Container = styled.div`
     @media (min-width: 1200px) {
         max-width: 1140px;
     }
-
-    width: 100%;
-    margin-left: auto;
-    margin-right: auto;
 `;
 
 const ProfileContainer = styled(Container)`
     display: flex;
     align-itmes: center;
     border: 1px solid purple;
-    padding: 1.8rem;
+    padding: 1.6rem;
+    background: #282830;
+    color: white;
+
+    @media (max-width: 768px) {
+        flex-wrap: wrap;
+        padding: 0.8rem;
+
+        .profile-img {
+            width: 4.8rem;
+            height: 4.8rem;
+        }
+        
+        .profile-desc span {
+            font-size: 1.2rem;
+        }
+    }
+`;
+const ProfileImgDesc = styled.div`
+    display: flex;
+    @media (max-width: 768px) {
+        margin: auto;
+    }
 `;
 
 const ProfileImg = styled.div`
     width: 6.4rem;
     height: 6.4rem;
+    flex: 1 0 auto;
 `;
 
 const ProfileDesc = styled.div`
-    margin: 0 0 0 1.2rem; 
+    margin: auto 0 auto 1.2rem; 
     font-weight: 900;
     
     span {
-        font-size: 2.4rem;
+        font-size: 2rem;
     }
 `;
 
-const ProfileRank = styled.div`
-    margin-left: auto;
+const ProfileRankContainer = styled.div`
+    margin: auto 0 auto auto;
+
+    @media (max-width: 768px) {
+        margin: auto;
+    }
 `;
 
 interface Props {
+    
+}
+
+const apiData = {
+    maxMmr: 10,
+    death: 10,
+    rank: 10,
+    maxRank:10,
+    kills:10,
+    updateTime: "now",
+    abandons: 10,
+    mmr:10,
+    wins:10,
+    region:"apac",
+    season:10,
+    losses:10
 }
 
 export default class Profile extends React.Component<Props> {
@@ -69,20 +109,21 @@ export default class Profile extends React.Component<Props> {
     render() {
         return (
             <ProfileContainer>
-                <ProfileImg className="profile-img" style={{
-                }}>
-                    <img src="https://ubisoft-avatars.akamaized.net/beec3d7b-a925-48a0-94bd-9896541dcbd5/default_256_256.png" style={{
-                        width: "100%"
-                    }}></img>
-                </ProfileImg>
+                <ProfileImgDesc>
+                    <ProfileImg className="profile-img">
+                        <img src="https://ubisoft-avatars.akamaized.net/beec3d7b-a925-48a0-94bd-9896541dcbd5/default_256_256.png" style={{
+                            width: "100%"
+                        }}></img>
+                    </ProfileImg>
 
-                <ProfileDesc className="profile-desc" style={{
-                }}>
-                    <span>Piliot(User Id)</span>
-                </ProfileDesc>
-                <ProfileRank>
-                   <h1> Current rank image</h1> 
-                </ProfileRank>
+                    <ProfileDesc className="profile-desc" style={{
+                    }}>
+                        <span>Piliot(User Id)</span>
+                    </ProfileDesc>
+                </ProfileImgDesc>
+                <ProfileRankContainer>
+                    <ProfileRank api={apiData} />
+                </ProfileRankContainer>
             </ProfileContainer>
         );
     }
