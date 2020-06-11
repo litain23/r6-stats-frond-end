b92c33cab1868a0fdad08f8dbda39ca873155e3d
Jiwoo Choi <pelomon2@gmail.com>
2020-06-10 19:16:20 -0500

    테스트코드 구성.

--- src/Pages/SearchPage/Seasons.tsx
+++ src/Pages/SearchPage/Seasons.tsx
@@ -25,7 +25,6 @@ export default class SearchSeasonsTab extends React.Component<Props> {
     }
 
     cardGenerator(title: string, items : RANKTEMPAPI[]) {
-
         let result = [];
         for( let i = 0; i < items.length ; i++) {
             const item = items[i];
